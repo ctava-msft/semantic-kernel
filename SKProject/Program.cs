f504902a8d4ddf1df60ec05e917455ccbf891ebd
Chris Tava <christava@microsoft.com>
2024-08-26 17:56:51 -0400

    init: program

---
+++ SKProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.SemanticKernel;
+
+var builder = Kernel.CreateBuilder();
+builder.Services.AddAzureOpenAIChatCompletion(
+"your-resource-name",
+"your-endpoint",
+"your-resource-key",
+"deployment-model");
+
+var kernel = builder.Build();
+
+var result = await kernel.InvokePromptAsync(
+"Give me a list of breakfast foods with eggs and cheese");
+Console.WriteLine(result);
---
+++ readme.md
@@ -0,0 +1,4 @@
+dotnet new console -o SKProject
+dotnet add package Microsoft.SemanticKernel --version 1.2.0
+dotent restore
+dotnet run --project SKProject.csproj
